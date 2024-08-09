33dbcc9356c96b68a21f436e70b7f57850f6790e
Xiaotian Wu <729406307@qq.com>
2024-08-09 19:06:40 +0800

    fetch data in backend

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const UserPage = () => {
-  return <div>UserPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const uses: User[] = await res.json();
+
+  return (
+    <>
+      <h1>UserPage</h1>
+      <ul>
+        {uses.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UserPage;
