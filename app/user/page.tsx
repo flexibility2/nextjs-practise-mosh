66049603fc2f6f56a4a76eebecd4b07039015ad1
Xiaotian Wu <729406307@qq.com>
2024-08-09 19:44:40 +0800

    test cache

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -6,12 +6,20 @@ interface User {
 }
 
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+  //   next: { revalidate: 10  },
+  // });
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users", {});
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+
   const uses: User[] = await res.json();
 
   return (
     <>
       <h1>UserPage</h1>
+      <div>{new Date().toISOString()}</div>
       <ul>
         {uses.map((user) => (
           <li key={user.id}>{user.name}</li>
