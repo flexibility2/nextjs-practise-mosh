0b5d220907122921128cdfcee09b88ba193b469a
Xiaotian Wu <729406307@qq.com>
2024-08-12 14:29:37 +0800

    use css tailwind

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import AddChart from "./AddChart";
-
+import styles from "./ProductCard.module.css";
 const ProductCard = () => {
   return (
-    <div>
+    // <div className={styles.card}>
+    //   <AddChart></AddChart>
+    // </div>
+    <div className="p-5 bg-sky-300 my-5 hover:bg-white text-gray-400 text-xl">
       <AddChart></AddChart>
     </div>
   );
