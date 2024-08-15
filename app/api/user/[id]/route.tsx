9d8ba6eb678ae397fb5831d6cdbacda11ab1fa14
Xiaotian Wu <729406307@qq.com>
2024-08-15 14:56:42 +0800

    use prisma get data from DB

--- app/api/user/[id]/route.tsx
+++ app/api/user/[id]/route.tsx
@@ -1,16 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import React from "react";
 import { User } from "../schema";
+import prisma from "@/prisma/client";
 
 interface Props {
-  params: { id: number };
+  params: { id: string };
 }
 
-export function GET(request: NextRequest, params: Props) {
-  if (params.params.id > 10) {
-    return NextResponse.json({ error: "id is too large" });
+export async function GET(request: NextRequest, params: Props) {
+  // if (params.params.id > 10) {
+  //   return NextResponse.json({ error: "id is too large" });
+  // } else {
+  //   return NextResponse.json({ id: params.params.id, name: "te" });
+  // }
+  const res = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.params.id),
+    },
+  });
+  if (res) {
+    return NextResponse.json(res);
   } else {
-    return NextResponse.json({ id: params.params.id, name: "te" });
+    return NextResponse.json({ error: "not found" }, { status: 404 });
   }
 }
 
@@ -29,7 +40,7 @@ export async function PUT(request: NextRequest, params: Props) {
   if (!body.name) {
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
   }
-  if (params.params.id > 10) {
+  if (parseInt(params.params.id) > 10) {
     return NextResponse.json({ error: "not found" }, { status: 404 });
   } else {
     return NextResponse.json({ id: params.params.id, name: body.name });
@@ -37,7 +48,7 @@ export async function PUT(request: NextRequest, params: Props) {
 }
 
 export async function DELETE(request: NextRequest, params: Props) {
-  if (params.params.id > 10) {
+  if (parseInt(params.params.id) > 10) {
     return NextResponse.json({ error: "not found" }, { status: 404 });
   } else {
     return NextResponse.json({});
--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -1,9 +1,16 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import React from "react";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "te" },
-    { id: 2, name: "aa" },
-  ]);
+export async function GET(request: NextRequest) {
+  // return NextResponse.json([
+  //   { id: 1, name: "te" },
+  //   { id: 2, name: "aa" },
+  // ]);
+  const res = await prisma.user.findMany();
+  if (res) {
+    return NextResponse.json(res);
+  } else {
+    return NextResponse.json({ error: "not found" }, { status: 404 });
+  }
 }
