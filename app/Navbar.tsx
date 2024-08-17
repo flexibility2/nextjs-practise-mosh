83b9264d0a315c49b9a932f9621dc8c6c276fe92
Xiaotian Wu <729406307@qq.com>
2024-08-17 21:54:46 +0800

    signout

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -12,7 +12,14 @@ const Navbar = () => {
       </Link>
       <Link href={"/user"}>UserPage</Link>
       {status === "loading" && <div>Loading...</div>}
-      {status === "authenticated" && <div>{session.user?.name}</div>}
+      {status === "authenticated" && (
+        <div>
+          {session.user?.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Logout
+          </Link>
+        </div>
+      )}
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Login</Link>
       )}
