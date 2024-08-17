e08ce3580ce656d6608a3730efa85de69053683b
Xiaotian Wu <729406307@qq.com>
2024-08-17 17:17:44 +0800

    use google auth

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,11 +3,12 @@ import React, { ReactNode } from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex bg-slate-400 p-5">
+    <div className="flex bg-slate-400 p-5 space-x-3">
       <Link className="mr-5" href={"/"}>
         Next.js
       </Link>
       <Link href={"/user"}>UserPage</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
