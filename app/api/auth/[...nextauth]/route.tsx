a670149ddabf608cadb3d80179d394077b5b850d
Xiaotian Wu <XWu32@slb.com>
2024-08-22 13:56:01 +0800

    use auth adapter

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,14 +1,19 @@
 import exp from "constants";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  // session: {
+  //   strategy: "jwt",
+  // },
 };
 
 const handler = NextAuth(authOptions);
