939b2ec0d7ae7c20c58117732e2c644781219545
Xiaotian Wu <729406307@qq.com>
2024-08-23 17:48:13 +0800

    use credentials

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -3,9 +3,35 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bycrpt from "bcrypt";
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user) return null;
+        const pwdMatch = await bycrpt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
+        if (pwdMatch) return user;
+        else return null;
+      },
+    }),
+
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
